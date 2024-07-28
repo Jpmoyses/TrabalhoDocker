c503d36e25f7df20b9192e32d8a429b26bd9a012
Jpmoyses <jpmoyses00@gmail.com>
2024-07-28 15:09:52 -0300

    Index atualizado

--- Trabalho Git/php/crudUsuario.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'conexao.php';
-function buscarUsuario($nome)
-{
-    connect();
-    $resultados = query("SELECT * FROM dono WHERE nome='$nome'");
-    close();
-    return $resultados;
-}
--- Trabalho Git/php/fazerLogin.php
+++ Trabalho Git/php/fazerLogin.php
@@ -1,5 +1,22 @@
+<?php
+$servername = "db";
+$username = "root";
+$password = "1234";
+$dbname = "BancoTrabalho";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Erro: " . $conn->connect_error);
+}
+echo "Conectado.";
+
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
+
 
 
 <head>
